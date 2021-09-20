cf7e924c268c5fdb4763f27887ae824fbea6459f
DESKTOP-RBVR18R\jang ye rim <yerim@mailplug.co.kr>
2021-09-20 19:49:37 +0900

    초기화

--- html/flie/nescafe/js/sub1script.js
+++
@@ -1,7 +0,0 @@
-$(function(){
- $('.sis').click(function(){
-   $(this).children('.sis-inner').stop().slideToggle(); 
-   $(this).siblings().children('.sis-inner').stop().slideUp();
-   return false;
- });
-});
